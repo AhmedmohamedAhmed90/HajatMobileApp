96177213065553b0cbfec4fd2e204601bcff030f
fathy2028 <fathy2111262@miuegypt.edu.eg>
2024-12-18 18:37:09 +0200

    splash screen and pubspec asset config

---
+++ lib/src/featrues/splash/ui/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset(
+          'assets/animations/loading.json',
+          width: 100,
+          height: 100,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
